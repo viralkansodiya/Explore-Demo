e6022fd06ba81a111df0a4c028209094ec7f15ef
viralkansodiya <viral@fosserp.com>
2025-05-18 02:53:27 +0000

    ch

--- explore_demo/explore_demo/redirect.py
+++ explore_demo/explore_demo/redirect.py
@@ -3,18 +3,12 @@ import requests
 
 @frappe.whitelist()
 def generate_redirect_url():
-    # try:
     doc = frappe.get_doc("Explore Demo Settings")
     
     if not doc.site_name:
         frappe.throw("Site Name is not set in explore demo settings")
     
-    # Prepare API credentials
-    api_key = doc.api_key
-    api_secret = doc.get_password('api_secret')
-    
-    # Construct the API URL
-    url = f"{doc.site_name}/api/method/explore_demo.explore_demo.login_api.login"
+    url = f"{doc.site_name}/api/method/explore_demo.explore_demo.login_api.login_with_redirect"
     
     headers = {
             'Accept': 'application/json',
@@ -25,15 +19,13 @@ def generate_redirect_url():
         "usr" : doc.user_id,
         "pwd" : doc.get_password("password")
     }
-    
-    # Make the request
+
     response = requests.post(url, json=payload, headers=headers)
 
-    return response
-    # Check for successful response
+
     if response:
         return {
-            "site_url": doc.site_name,
+            "site_url": f"{doc.site_name}/api/method/explore_demo.explore_demo.login_api.login_with_redirect?usr={doc.user_id}&pwd={doc.get_password('password')}",
             "success": 1
         }
     else:
